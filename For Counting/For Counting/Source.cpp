f5f12f7003761ee61ee19b8ba63b12c60d5513fb
Burton428 <ADAMBURTON428@users.noreply.github.com>
2018-09-21 11:13:46 -0500

    updated

---
+++ For Counting/For Counting/Source.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main() {
+	
+	
+	/*for (int i = 0; i <= 100;i = i +3)
+	{
+		cout << i << endl;
+		if (i == 48)
+		{
+			cout << "50" << endl;
+			cout << "Want to here a joke...Sometimes I dream about cheese" << endl;
+		}
+
+	}
+	for (int x = 100; x >= 0; x = x -5)
+	{
+		cout << x << endl;
+		
+	}*/
+	
+	for (int z = 0 ; z <= 1000; z++)
+	{
+		if (z % 2 == 0 && z % 3 == 0 && z <= 12 ) 
+		{
+			cout << z << endl;
+		}
+	}
+	
+	
+	
+	
+	
+	
+	system("pause");
+	return 0;
+
+}
