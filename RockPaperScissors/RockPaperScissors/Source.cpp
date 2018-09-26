5c26e47fb5e9db96806dbdda557abfddcd9be79e
Burton428 <ADAMBURTON428@users.noreply.github.com>
2018-09-26 11:07:14 -0500

    update

---
+++ RockPaperScissors/RockPaperScissors/Source.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <time.h>
+#include <stdio.h>
+#include<stdlib.h>
+using namespace std;
+
+int main() {
+	int userChoice;
+	int computerChoice;
+	int Rock;
+	int Paper;
+	int Scissors;
+	srand(time(NULL));
+	computerChoice = rand() % 3 + 1;
+
+	 Rock = 1;
+	 Paper = 2;
+	 Scissors = 3;
+
+	 cout << "Rock \nPaper \nScissors"
+
+
+
+
+
+
+
+
+
+
+
+	system("pause");
+	return 0;
+}
