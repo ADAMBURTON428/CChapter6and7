85dc09d0ef30f725b9113197a62d4bfe99178fba
Burton428 <ADAMBURTON428@users.noreply.github.com>
2018-09-27 10:19:02 -0500

    updated

--- RockPaperScissors/RockPaperScissors/Source.cpp
+++ RockPaperScissors/RockPaperScissors/Source.cpp
@@ -5,25 +5,65 @@
 #include<stdlib.h>
 using namespace std;
 
-int main() {
+int main() 
+{
 	int userChoice;
 	int computerChoice;
-	int Rock;
-	int Paper;
-	int Scissors;
 	srand(time(NULL));
 	computerChoice = rand() % 3 + 1;
 
-	 Rock = 1;
-	 Paper = 2;
-	 Scissors = 3;
 
-	 cout << "Rock \nPaper \nScissors"
+	cout << "1-Rock \n2-Paper \n3-Scissors";
+	cin >> userChoice;
+
+	if (computerChoice == 1)
+	{
+		if (userChoice == 1)
+		{
+			cout << "Tie";
+		}
+		else if (userChoice == 2)
+		{
+			cout << "You win";
+		}
+		else
+		{
+			cout << "You lose";
+		}
 
 
+	}
+	else if (computerChoice == 2)
+	{
+		if (userChoice == 1)
+		{
+			cout << "You lose";
+		}
+		else if (userChoice == 2)
+		{
+			cout << "Tie";
+		}
+		else
+		{
+			cout << "You win";
+		}
+	}
+	else if (computerChoice == 3)
+	{
+		if (userChoice == 1)
+		{
+			cout << "You win";
+		}
+		else if (userChoice == 2)
+		{
+			cout << "You lose";
+		}
+		else
+		{
+			cout << "Tie";
+		}
 
-
-
+	}
 
 
 
